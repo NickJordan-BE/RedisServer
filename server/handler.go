90ebc4e519ece4abf14572e888c92e9faecbd97a
njordan <njordan1@uw.edu>
2025-04-20 20:43:43 -0700

    Feature: Implement Redis commands and handler

---
+++ server/handler.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+// Ping Command
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{typ: "string", str: "PONG"}
+	}
+
+	return Value{typ: "string", str: args[0].bulk}
+}
+
+// Echo Command
+func echo(args []Value) Value {
+	return Value{typ: "string", str: args[0].bulk}
+}
+
+// Set command
+func set(args []Value) Value {
+	if len(args) != 2 && len(args) != 4 {
+		return Value{typ: "error", str: "ERR: Wrong number of arguments for set command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	// Locking because of concurrent connections
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+
+	// Checks for expiration argmuments
+	if len(args) == 4 && strings.ToUpper(args[2].bulk) == "PX" {
+		if exp, err := strconv.Atoi(args[3].bulk); err == nil {
+			// Concurrent function sleeps for given milliseconds
+			// Then removes key from cache
+			go func(key string, duration int) {
+				time.Sleep(time.Duration(duration) * time.Millisecond)
+				delete(SETs, key)
+			}(key, exp)
+		}
+	}
+
+	return Value{typ: "string", str: "OK"}
+}
+
+// GET Command
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "string", str: "ERR: Wrong number of arguments for get command"}
+	}
+
+	key := args[0].bulk
+
+	// Lock due to multiple concurrent connections
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+// HSET command
+func hSet(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "string", str: "ERR: Wrong number of arguments for hset command"}
+	}
+
+	hashMap := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	// Lock due to multiple connections
+	HSETsMu.Lock()
+	if _, ok := HSETs[hashMap]; !ok {
+		HSETs[hashMap] = map[string]string{}
+	}
+	HSETs[hashMap][key] = value
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+// HGET command
+func hGet(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "string", str: "ERR: Wrong number of arguments for hget command"}
+	}
+
+	hashMap := args[0].bulk
+	key := args[1].bulk
+
+	// Lock due to multiple connections
+	HSETsMu.RLock()
+	value, ok := HSETs[hashMap][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+// HGETALL Command
+func hGetAll(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "string", str: "ERR: Wrong number of arguments for hgetall command"}
+	}
+
+	hashMap := args[0].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hashMap]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	var values = make([]Value, len(value)*2)
+	j := 0
+	for key, v := range value {
+		values[j] = Value{typ: "bulk", bulk: key}
+		values[j+1] = Value{typ: "bulk", bulk: v}
+		j += 2
+	}
+
+	return Value{typ: "array", array: values}
+}
+
+// handles function calls for commands
+var Handlers = map[string]func([]Value) Value{
+	"PING":    ping,
+	"ECHO":    echo,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hSet,
+	"HGET":    hGet,
+	"HGETALL": hGetAll,
+}
--- server/server.go
+++ server/server.go
@@ -5,11 +5,13 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Listening on port :6379")
 
+	// Establish port connection
 	server, err := net.Listen("tcp", ":6379")
 
 	if err != nil {
@@ -24,12 +26,15 @@ func main() {
 		return
 	}
 
+	// Close on end of connection
 	defer conn.Close()
 
 	for {
+		// Allow RESP to recieve requests
 		resp := NewResp(conn)
 
-		message, err := resp.Read()
+		// RESP serialize request into RESP array
+		value, err := resp.Read()
 
 		if err != nil {
 			if err == io.EOF {
@@ -39,9 +44,34 @@ func main() {
 			os.Exit(1)
 		}
 
-		_ = message
+		if value.typ != "array" {
+			fmt.Println("Invalid Request")
+			continue
+		}
 
+		if len(value.array) == 0 {
+			fmt.Println("Array cannot be empty.")
+			continue
+		}
+
+		// Connecting writer to port
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		// Decoding request from RESP array
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		// Checking command is valid and grabbing function
+		handler, ok := Handlers[command]
+
+		if !ok {
+			fmt.Println("Invalid Command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		// Responding to client
+		res := handler(args)
+		writer.Write(res)
 	}
 }
